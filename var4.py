d6f457069dd016ef4b546f284efb1d3fdf0eb943
ivan <ivangoglev@protonmail.com>
2020-03-04 22:53:47 +0300

    'Вот_итог_задания'

--- var4.py
+++ var4.py
@@ -1,6 +1,7 @@
 import datetime
 ZNAK=[]
-n=2
+n=8
+print('1) ввод элементов')
 for _ in range(n):
     a=[]
     name_fio=input('Введите фамилию и имя: ')
@@ -24,5 +25,59 @@ for i in range(l):
             ZNAK[i + 1]=temp
             l-=1
 
-
+# Вывод Znak
+print('2)Вывод информации')
 print(ZNAK)
+def RibaLevOutput(ZNK):
+    '''
+    данная функция выводит сотрудников чьи знаки riba или lev
+    '''
+    rezult=[]
+    for item in ZNK:
+        if item[1]=='lev' or item[1]=='riba':
+            rezult.append(item[0])
+
+    return rezult
+print('3)вывести сотрудников чьи знаки riba или lev')
+print(RibaLevOutput(ZNAK))
+
+def HumanInfoOutput(ZNK):
+    '''
+    данная функция выводит инфу о введённом сотруднике
+    '''
+    men = input('Введите человека:')
+    for item in ZNK:
+        if item[0]==men:
+            return item
+    return None
+print('4)вывести информацию о введённом сотруднике')
+print(HumanInfoOutput(ZNAK))
+
+def AugustBornCount(ZNK):
+    '''
+    данная функция выводит количество людей родившихся в августе
+    '''
+    count = 0
+    for item in ZNK:
+        if item[2][1] == 8:
+            count +=1
+    return count
+print('5)вывести количество людей родившихся в августе')
+print(AugustBornCount(ZNAK))
+
+
+def Delete30(ZNK):
+    '''
+        данная функция удаляет из списка людей родившихся 30 числа
+        '''
+    temp = 0
+    for item in ZNK:
+        if item[2][0] == 30:
+            ZNK.pop(temp)
+    temp +=1
+    return ZNK
+
+print('6)удалить из списка людей родившихся 30 числа')
+print(Delete30(ZNAK))
+
+
